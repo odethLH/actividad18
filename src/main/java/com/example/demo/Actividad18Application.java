fb6e39ef473a0bdc1b85faab50a8ed37662a0c76
ODETH <ODETH@DESKTOP-4NHG90Q>
2023-01-13 22:17:54 -0500

    Actividad 18

---
+++ src/main/java/com/example/demo/Actividad18Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad18Application.class, args);
+	}
+
+}
